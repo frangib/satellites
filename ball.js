c8320208d4e43d87ec0255d3cd5492556f54d181
frangib <44316116+frangib@users.noreply.github.com>
2021-10-06 13:40:14 +0200

    Compute orbital period once
    Now the orbital period is only computed once for every satellite instead of every setTimeout() in ball.js.
    Other errors fixed in user input and distances.

--- ball.js
+++ ball.js
@@ -5,6 +5,7 @@ let a = 0;
 let b = 0;
 let inclination = 0;
 let motion = 0;
+let orbitalPeriod = 0;
 
 onmessage = function (e) {
     a = e.data.a;
@@ -12,33 +13,25 @@ onmessage = function (e) {
     phaseShift = e.data.phaseShift;
     inclination = e.data.inclination;
     motion = e.data.motion;
-    originalA = a;
+    orbitalPeriod = e.data.orbitalPeriod
+
 }
 
 function position() {
     //Current time in seconds:
     const t = Date.now() / 1; 
 
-    // Satellites orbital period:
-    const T = orbitalPeriod(a);
-
-    const theta = 2*Math.PI*t/T + phaseShift;
+    const theta = 2*Math.PI*t/orbitalPeriod + phaseShift;
     const pos = { 
         "x": -motion*a * Math.cos(theta),
         //TODO: y axis
-        "y": 0,
+        "y": 0,//b*Math.cos(theta)
         "z": b * Math.sin(theta)
     };
 
     
     postMessage(pos);
     setTimeout("position()", 100);
-}
-
-function orbitalPeriod(heightAboveSurface){
-    const MU = 3.986004418e+14;
-    let period = 2*Math.PI*Math.sqrt(((RADIUS_EARTH + heightAboveSurface))**3/(MU));
-    return period;
 }
 
 position();
